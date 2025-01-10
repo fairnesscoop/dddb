726c0501b5d0356ec08c413060f192b746712db6
Gregory Pelletier <gregory.pelletier@fairness.coop>
2025-01-10 19:12:58 +0100

    upgrade composer dependencies

--- src/Infrastructure/Persistence/Doctrine/Fixtures/SerieFixture.php
+++ src/Infrastructure/Persistence/Doctrine/Fixtures/SerieFixture.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Persistence\Doctrine\Fixtures;
 
+use App\Domain\Model\Manufacturer;
 use App\Domain\Model\Serie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -11,14 +12,14 @@ use Doctrine\Persistence\ObjectManager;
 
 final class SerieFixture extends Fixture implements DependentFixtureInterface
 {
-    public function getDependencies()
+    public function getDependencies(): array
     {
         return [ManufacturerFixture::class];
     }
 
     public function load(ObjectManager $manager): void
     {
-        $fairphone = $this->getReference('fairphone');
+        $fairphone = $this->getReference('fairphone', Manufacturer::class);
         $fairphone4 = new Serie(
             '12134585-b6c4-444b-8dc3-2a17a0693e5c',
             'Serie 4',
@@ -27,7 +28,7 @@ final class SerieFixture extends Fixture implements DependentFixtureInterface
 
         $manager->persist($fairphone4);
 
-        $fairphone = $this->getReference('fairphone');
+        $fairphone = $this->getReference('fairphone', Manufacturer::class);
         $fairphone3 = new Serie(
             '963c1b4e-7a0c-452e-88a0-d3a583f78b8e',
             'Serie 3',
@@ -36,7 +37,7 @@ final class SerieFixture extends Fixture implements DependentFixtureInterface
 
         $manager->persist($fairphone3);
 
-        $google = $this->getReference('google');
+        $google = $this->getReference('google', Manufacturer::class);
         $pixel = new Serie(
             'fe9881a2-58a0-40ff-acac-14c276ce15f1',
             'Pixel',
